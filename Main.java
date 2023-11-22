f1b53907e868c7ba35f48ae1e4e4f89707d729a7
JorgeNeusch <142831073+JorgeNeusch@users.noreply.github.com>
2023-11-21 19:18:34 -0600

    Update Main.java
    added a quick menu option, to be added to later

--- Main.java
+++ Main.java
@@ -1,21 +1,57 @@
+import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Main {
+	
+	
+	public static void printMenu() {
+		System.out.println("Menu options:");
+		System.out.println("P = Play a game of sudoku.");
+		System.out.println("Q = Quit game");
+		System.out.print("Please enter an option: ");
+	}
+
 
 	public static void main(String[] args) {
 		
-		Sudoku sud = new Sudoku();
-		sud.printSudoku();
-		System.out.println();
-		System.out.println();
-		System.out.println();
+		Scanner input = new Scanner(System.in);
+		LinkedList<Sudoku> games = new LinkedList<Sudoku>();
+		char option;
+		byte numGames = 0;
 		
-		sud.fillSudoku();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		sud.printSudoku();
+		System.out.println("Hello, welcome to Sudoku in Java!");
 		
 		
+		do {
+			
+			printMenu();
+			
+			option = input.next().charAt(0);
+			option = Character.toUpperCase(option);
+			
+			switch (option) {
+			
+				case 'P' :
+				
+					games.add(new Sudoku());
+					games.get(numGames).fillSudoku();
+					games.get(numGames).printSudoku();
+					++numGames;
+					break;
+					
+				case 'Q' :
+					System.out.println("Thanks for playing");
+					break;
+				
+				default :
+					System.out.println("Unknown command");
+					
+			}
+			
+			
+		} while(option != 'Q');
+		
+		input.close();
 		
 	}
 
